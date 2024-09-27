4df69c5a9fa332ca73fd11fe30a1614006e745e8
Marley <Marley@ENROXES>
2024-09-27 23:23:02 +0500

    Добавьте файлы проекта.

---
+++ pr2_c++_zad4.cpp
@@ -0,0 +1,41 @@
+﻿using namespace std;
+#include <iostream>
+
+
+class Rectancle 
+{
+private:
+    double width;
+    double height;
+public:
+
+    double Ploshad() const
+    {
+        return width * height;
+    }
+
+    void setWidth(double w) 
+    {
+        width = w;
+    }
+
+    void setHeight(double h) 
+    {
+        height = h;
+    }
+};
+
+int main()
+{
+    setlocale(0, "rus");
+    double width;
+	double height;
+	cout << "Введите ширину прямоугольника: ";
+	cin >> width;
+	cout << "Введите длину прямоугольника: ";
+	cin >> height;
+	Rectancle rectangle;
+	rectangle.setWidth(width);
+	rectangle.setHeight(height);
+    cout << "Площадь прямоугольника равна: " << rectangle.Ploshad() << endl;
+}
